1faf7e1cb367069ede5e9076131fb1f3de30f6ca
Allanflo88 <allanfloresdejesus@hotmail.com>
2017-12-27 17:13:58 -0200

    Add First Hello World with Spring framework

---
+++ HelloSpring/src/com/allan/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.allan;
+
+public class HelloWorld {
+	private String msg;
+
+	public void getMsg() {
+		System.out.println("Your message : " + msg);
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+	
+
+}
